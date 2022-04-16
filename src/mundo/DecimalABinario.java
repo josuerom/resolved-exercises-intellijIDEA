6ae9c019376ba4c7f9509c33fe83925fad4288c4
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-04-16 16:45:10 -0500

    Ejercicios resueltos 👨‍💻

--- src/mundo/DecimalABinario.java
+++ src/mundo/DecimalABinario.java
@@ -1,23 +1,33 @@
 package mundo;
 
 import java.util.Scanner;
-
 public class DecimalABinario {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("A qué número desea conocerle su binario -> ");
+        System.out.print("A qué número desea conocerle el binario -> ");
         int decimal = sc.nextInt();
-        int acc = 0, el = 0;
+        int acc = decimal, i = 1, el = 0;
+        String invertida = "", binario = "";
 
-        for (int i = 0; i < decimal; i++) {
-            acc = decimal / 2;
-
-            if (decimal % 2 == 0) {
-
-            } else if (decimal %2 == 1) {
+        while (i <= acc) {
+            if (acc % 2 == 0) {
+                invertida += "0";
+                el++;
+            } else if (acc % 2 == 1) {
+                invertida += "1";
+                el++;
 
             }
+            // Cada 4 items agrego añado un espacio
+            if (el % 4 == 0) {
+                invertida += " ";
+            }
+            acc = acc / 2;
         }
+        // Invierto la cadena para que quede de la forma correcta
+        for (int indice = invertida.length() - 1; indice >= 0; indice--) {
+            binario += invertida.charAt(indice);
+        }
+        System.out.println(decimal + " en binario es -> " + binario);
     }
 }
